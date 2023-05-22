c805d44e27db395f5ff172147551a7ffc1fb5433
Lucas Martin Mota <lucas127mm@gmail.com>
2023-05-22 17:20:25 -0300

    seccondCommit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '', component: MainPageComponent
+},
+{
+  path: 'about', component: AboutPageComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GlobalModule } from './global.module';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { TopPageComponent } from './pages/top-page/top-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    TopPageComponent,
+    HomePageComponent,
+    AboutPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    GlobalModule
   ],
   providers: [],
   bootstrap: [AppComponent]
